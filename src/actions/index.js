c50994edf1ee61c87f4eb8b69b95393c04179018
chrystalme <afam_ifeanyi@live.com>
2021-07-29 16:24:03 +0300

    Update books reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,12 @@
 export const CREATE_BOOK = 'CREATE_BOOK';
 export const REMOVE_BOOK = 'REMOVE_BOOK';
 
-export const addBook = book => {
-  return {
-    type: CREATE_BOOK,
-    payload: book,
-  };
-};
+export const addBook = book => ({
+  type: CREATE_BOOK,
+  payload: book,
+});
 
-export const removeBook = book => {
-  return {
-    type: REMOVE_BOOK,
-    payload: book.index,
-  };
-};
+export const removeBook = book => ({
+  type: REMOVE_BOOK,
+  payload: book.index,
+});
