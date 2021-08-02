2e071dc060bb5c3f2bec470974cc45badce4c3f9
chrystalme <afam_ifeanyi@live.com>
2021-08-02 18:08:53 +0300

    Add removeBook props to Book component and validated removeBook props

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Book = ({ book }) => (
+const Book = ({ book, removeBook }) => (
   <tr>
     <td>{book.ISBN}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
+    <td>
+      <button type="button" onClick={() => removeBook(book.ISBN)}>Remove Book</button>
+    </td>
   </tr>
 );
 
 Book.propTypes = {
   book: PropTypes.instanceOf(Object).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
-export default Book;
+const ConnectedComponent = connect()(Book);
+
+export default ConnectedComponent;
