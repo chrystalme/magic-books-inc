e65544b01795d96a67fdadf27d9f99c8c6c44d75
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-04 18:06:13 +0500

    Update BookList.js adding handleFilterChange logic

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,34 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
-import { removeBook } from '../actions';
+import { removeBook, changeFilter } from '../actions';
+import CategoryFilter from '../components/CategoryFilter';
 
-const BooksList = ({ books, removeBook }) => {
+const BooksList = ({
+  books, removeBook, changeFilter, filter,
+}) => {
   const handleRemoveBook = (ISBN) => {
     removeBook(ISBN);
   };
+
+  const handleFilterChange = (filter) => {
+    changeFilter(filter);
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>Book ID</td>
-          <td>Title</td>
-          <td>Category</td>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((book) => (
-          <Book key={book.ISBN} book={book} removeBook={handleRemoveBook} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <CategoryFilter handleFilter={handleFilterChange} />
+      <table>
+        <thead>
+          <tr>
+            <td>Book ID</td>
+            <td>Title</td>
+            <td>Category</td>
+          </tr>
+        </thead>
+        <tbody>
+          {books.filter((book) => (filter === 'All' ? books : book.category === filter))
+            .map((book) => (
+              <Book key={book.ISBN} book={book} removeBook={handleRemoveBook} />
+            ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
-const mapStateToProps = (state) => ({ books: state.Books });
+const mapStateToProps = (state) => ({
+  books: state.Books,
+  filter: state.filter,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   removeBook: (ISBN) => dispatch(removeBook(ISBN)),
+  changeFilter: (filter) => dispatch(changeFilter(filter)),
 });
 
 const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(BooksList);
@@ -37,6 +53,8 @@ const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(BooksLis
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object).isRequired,
   removeBook: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  changeFilter: PropTypes.func.isRequired,
 };
 
 export default ConnectedComponent;
