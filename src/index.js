362e68d8a2226ae379722f538a577e4e3594e813
chrystalme <afam_ifeanyi@live.com>
2021-08-04 17:39:25 +0300

    Fix Linter

--- src/index.js
+++ src/index.js
@@ -30,9 +30,7 @@ const initialState = {
   ],
 };
 
-const store = createStore(rootReducer, initialState,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
