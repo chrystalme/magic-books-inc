15b981649403eb1cebc243eb33f8dec15bc9666d
chrystalme <afam_ifeanyi@live.com>
2021-08-04 16:27:46 +0300

    Add filterReducer

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,13 +1,13 @@
-import { CHANGE_FILTER } from "../actions";
+import { CHANGE_FILTER } from '../actions';
 
-const changeReducer = (state='All', action) => {
-    switch (action.type) {
-        case CHANGE_FILTER:
-            return action.payload
-    
-        default:
-            return state;
-    }
-}
+const changeReducer = (state = 'All', action) => {
+  switch (action.type) {
+    case CHANGE_FILTER:
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
 
 export default changeReducer;
